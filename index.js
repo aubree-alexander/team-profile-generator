63ecc5ec6556ec48da38851245f7d5d74ec838df
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-01 22:22:13 -0500

    more file setup, initial function setup

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const questions = [
+{
+    //questions could go here, but how to accomodate for different questions needed for different types of individuals?
+}
+
+];
+
+
+// TODO: Create a function to initialize app
+function init() {
+    
+    inquirer.prompt(questions)
+    .then(function(response) {
+        
+        return fs.writeFileSync('index.html', generateProfiles(response), function(err) {
+            if (err) {
+                throw err;
+            };
+        });
+    })
+}
+
+
+function generateProfiles(data) {
+    return 
+    //put html setup in a template literal here
+  }
+
+// Function call to initialize app
+init();
+
